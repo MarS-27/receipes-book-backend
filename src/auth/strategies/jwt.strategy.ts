50c5c29cf7b5995cfe522ae1bdb4b94789ba2037
MarS-27 <marchuk1992@gmail.com>
2024-02-19 17:08:25 +0200

    changed img on imgPath in jwt-strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -18,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     return {
       id: user.id,
       email: user.email,
-      img: user.imgPath,
+      imgPath: user.imgPath,
       userName: user.userName,
     };
   }
